fe3b253864e5d0d4cd2450650df825d6a4dfc4e2
Corban <sonnemanc@gmail.com>
2021-10-29 14:11:54 -0600

    Done with Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Mr. Chocolate",10,"M","Brown","Lazy",True);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Rowdy",13,"M","Dark Brown","Curious",True);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Tabitha",7,"F","Light Brown","Bubbly",True);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Sergeant Brown",11,"M","Light Brown","Curious",True);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Melissa",14,"F","Dark Brown","Moody",True);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Grinch",5,"F","Brown","Needy",True);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES ("Wendy",12,"F","Light Brown","Lazy",True);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES (NULL,17,"M","Brown","Moody",False);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT bears.name, bears.age FROM bears WHERE sex = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name FROM bears ORDER BY name ASC"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT bears.name, bears.age FROM bears WHERE alive = True ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, MAX(age) FROM bears"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, MIN(age) FROM bears"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, max(ColorCount) FROM bears, (SELECT count(color) AS ColorCount FROM bears GROUP BY color);"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT count(temperament) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT id, name, age, sex, color, temperament, alive FROM bears WHERE name is null"
 end
